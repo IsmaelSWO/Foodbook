8f8edb2fe347f23ad76cf622d4484a9c4a52c45d
IsmaelSWO <ismael.ulalam@softwareone.com>
2021-10-11 13:48:41 +0200

    changes

---
+++ README.md
@@ -0,0 +1,6 @@
+# Foodbook Project
+
+Web application with custom recipe uploads.
+
+Check for a live demo here:
+https://foodbook-ismael.netlify.app/#5ed6604591c37cdc054bcd09
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e389bfb5-3f62-4028-99df-311335468869';
+export const MODAL_CLOSE_SEC = 2.5;
